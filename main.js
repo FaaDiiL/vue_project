5b4976fe1ff1e0e5ac367487a4ad8d07338a44fc
FaaDiiL <fadil.alfarhan@gmail.com>
2022-05-28 16:27:27 +0200

    L5-end

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 const app = Vue.createApp({
      data() {
-         return {
-             product: 'Socks',
-             image: './assets/images/socks_green.jpg',
-             inStock: true,
-             inventory: 100,
-             style:{many:'many',some:'some',none:'none'},
-             onSale: false
-         }
+          return {
+               product: 'Socks',
+               image: './assets/images/socks_green.jpg',
+               inStock: true,
+               inventory: 100,
+               style:{many:'many',some:'some',none:'none'},
+               onSale: false,
+               details: ['70% cotton', '10% polyester', '20% wool'],
+               variants:[
+                    { id: 1234, color:'green' },
+                    { id: 1235, color:'blue' }
+               ],
+               sizes: ['S', 'M', 'L', 'XL'],
+          }
      }
  })
